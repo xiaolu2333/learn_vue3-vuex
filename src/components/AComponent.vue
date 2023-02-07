b61afbe7095f19314bdcc5b10d642657209dfec6
xiaolu2333 <2694977226@qq.com>
2023-02-07 21:51:50 +0800

    核心概念 store 之获取 Vuex 状态：使用 mapState 辅助函数在计算属性中一次性计算多个状态。
    如果计算属性的名称与 state 的子节点名称相同时，则可以给 mapState 传一个字符串数组。

--- src/components/AComponent.vue
+++ src/components/AComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div>
-      <p>{{ storeStateOne }}</p>
-      <p>{{ storeStateTwo }}</p>
-      <p>{{ storeStateThree }}</p>
+      <p>{{ stateOne }}</p>
+      <p>{{ stateTwo }}</p>
+      <p>{{ stateThree }}</p>
     </div>
-    <span>AComponent: {{ storeCountState }}</span>
+    <span>AComponent: {{ count }}</span>
   </div>
 </template>
 
@@ -16,55 +16,18 @@ export default {
   name: 'AComponent',
   data() {
     return {
-      localCount: 1,
     }
   },
-  // computed: {
-  //   // 如果有多个状态，则需要在 computed 中添加多个 getter
-  //   storeCountState() {
-  //     return this.$store.state.count;
-  //   },
-  //   storeStateOne() {
-  //     return this.$store.state.stateOne;
-  //   },
-  //   storeStateTwo() {
-  //     return this.$store.state.stateTwo;
-  //   },
-  //   storeStateThree() {
-  //     return this.$store.state.stateThree;
-  //   },
-  // },
+  computed: mapState(
+      // 如果计算属性的名称与 state 的子节点名称相同时，则可以给 mapState 传一个字符串数组。
+      [
+        'count',  // map this.count to store.state.count
+        'stateOne',
+        'stateTwo',
+        'stateThree'
+      ]
+  )
 
-  // 使用 mapState 辅助函数一次性计算多个状态，
-  // // 方式一：使用箭头函数
-  // computed: mapState({
-  //   storeCountState: state => state.count,
-  //   storeStateOne: state => state.stateOne,
-  //   storeStateTwo: state => state.stateTwo,
-  //   storeStateThree: state => state.stateThree,
-  // })
-  // // 方式二：传字符串参数，简化箭头函数
-  // computed: mapState({
-  //   storeCountState: 'count', // 等同于 storeCountState: state => state.count
-  //   storeStateOne: 'stateOne',
-  //   storeStateTwo: 'stateTwo',
-  //   storeStateThree: 'stateThree',
-  // })
-  // 方式三：为了能够使用 `this` 获取组件局部的状态，必须使用常规函数。然后在 template 中使用 this.storeCountState() 调用
-  computed: mapState({
-    storeCountState(state) {
-      return state.count + this.localCount
-    },
-    storeStateOne(state) {
-      return state.stateOne
-    },
-    storeStateTwo(state) {
-      return state.stateTwo
-    },
-    storeStateThree(state) {
-      return state.stateThree
-    },
-  }),
 }
 </script>
 
