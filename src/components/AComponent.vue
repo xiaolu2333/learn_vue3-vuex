bbe557fd3540cc6ac7673faa94bb0084b8f3131b
xiaolu2333 <2694977226@qq.com>
2023-02-07 21:45:45 +0800

    核心概念 store 之获取 Vuex 状态：使用 mapState 辅助函数在计算属性中一次性计算多个状态。

--- src/components/AComponent.vue
+++ src/components/AComponent.vue
@@ -1,23 +1,70 @@
 <template>
   <div>
+    <div>
+      <p>{{ storeStateOne }}</p>
+      <p>{{ storeStateTwo }}</p>
+      <p>{{ storeStateThree }}</p>
+    </div>
     <span>AComponent: {{ storeCountState }}</span>
   </div>
 </template>
 
 <script>
-// 不再需要导入 store，因为我们可以在 this.$store 中访问它
+import { mapState } from 'vuex'
 
 export default {
   name: 'AComponent',
   data() {
     return {
+      localCount: 1,
     }
   },
-  computed: {
-    storeCountState() {
-      return this.$store.state.count;
-    }
-  },
+  // computed: {
+  //   // 如果有多个状态，则需要在 computed 中添加多个 getter
+  //   storeCountState() {
+  //     return this.$store.state.count;
+  //   },
+  //   storeStateOne() {
+  //     return this.$store.state.stateOne;
+  //   },
+  //   storeStateTwo() {
+  //     return this.$store.state.stateTwo;
+  //   },
+  //   storeStateThree() {
+  //     return this.$store.state.stateThree;
+  //   },
+  // },
+
+  // 使用 mapState 辅助函数一次性计算多个状态，
+  // // 方式一：使用箭头函数
+  // computed: mapState({
+  //   storeCountState: state => state.count,
+  //   storeStateOne: state => state.stateOne,
+  //   storeStateTwo: state => state.stateTwo,
+  //   storeStateThree: state => state.stateThree,
+  // })
+  // // 方式二：传字符串参数，简化箭头函数
+  // computed: mapState({
+  //   storeCountState: 'count', // 等同于 storeCountState: state => state.count
+  //   storeStateOne: 'stateOne',
+  //   storeStateTwo: 'stateTwo',
+  //   storeStateThree: 'stateThree',
+  // })
+  // 方式三：为了能够使用 `this` 获取组件局部的状态，必须使用常规函数。然后在 template 中使用 this.storeCountState() 调用
+  computed: mapState({
+    storeCountState(state) {
+      return state.count + this.localCount
+    },
+    storeStateOne(state) {
+      return state.stateOne
+    },
+    storeStateTwo(state) {
+      return state.stateTwo
+    },
+    storeStateThree(state) {
+      return state.stateThree
+    },
+  }),
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,10 @@ const store = createStore({
     state() {           // state 是一个函数，返回一个对象，这个对象就是我们的状态树，这个对象可以包含任意的属性，这些属性就是我们的状态
         "use strict";
         return {
-            count: 0
+            count: 0,
+            stateOne: 'stateOne',
+            stateTwo: 'stateTwo',
+            stateThree: 'stateThree',
         };
     },
     mutations: {        // mutations 是一个对象，这个对象包含了我们的状态变更函数，这些函数就是我们的状态变更行为，这些函数接收两个参数，第一个参数是 state，第二个参数是 payload，payload 是我们提交状态变更函数时传递的参数
