bf5bdd4fcc8571bd48ec7c4ada630835e752025f
xiaolu2333 <2694977226@qq.com>
2023-02-08 13:12:30 +0800

    核心概念 action ：注册 action。

--- src/store/index.js
+++ src/store/index.js
@@ -26,6 +26,17 @@ const store = createStore({
             console.log("decreased by " + payload.amount);
             state.count -= payload.amount;
         }
+    },
+
+    actions: {  // action 提交的是 mutation，而不是直接变更状态。context 参数是一个与 store 实例具有相同方法和属性的 context 对象，payload 是一个额外的对象参数。
+        increment(context, payload) {
+            // 通过 context.commit 提交 mutation：
+            context.commit(COUNT_INCREMENT_MUTATION, payload);
+        },
+        // 为了简化多次调用 context.commit，我们可以使用 ES2015 的参数解构：
+        decrement({commit}, payload) {
+            commit(COUNT_DECREMENT_MUTATION, payload);
+        }
     }
 });
 
