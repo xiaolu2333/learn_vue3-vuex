5cbe54c61d3d8b180a4b4c42c7e12a82d3da962b
xiaolu2333 <2694977226@qq.com>
2023-02-09 08:25:11 +0800

    核心概念 action ：分发 action。

--- src/components/AComponent.vue
+++ src/components/AComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <p>AComponent: {{ stateCount }}</p>
-    <button @click="increment({ amount: this.changeRange.increment })">Increase</button>
-    <button @click="decrement({ amount: this.changeRange.decrement})">Decrease</button>
+    <button @click="increment({ amount: this.changeRange.increment })">IncreaseByMutation</button>
+    <button @click="decrementCount">DecreaseByAction</button>
   </div>
 </template>
 
@@ -27,16 +27,15 @@ export default {
   },
   methods: {
     // 将组件中的 methods 映射为 store.commit 调用（需要在根节点注入 store）
-    // 方式一：使用数组
     ...mapMutations([
         COUNT_INCREMENT_MUTATION, // COUNT_INCREMENT_MUTATION 将被映射为 `this.$store.commit('increment')`
         COUNT_DECREMENT_MUTATION,
     ]),
-    // // 方式二：
-    // ...mapMutations({
-    //   increment: COUNT_INCREMENT_MUTATION,  // 将 `this.increment()` 映射为 `this.$store.commit('increment')`
-    //   decrement: COUNT_DECREMENT_MUTATION,
-    // }),
+
+    decrementCount() {
+      // 添加了载荷与 `this.$store.commit('decrement', { amount: this.changeRange.decrement })` 等效
+      this.$store.dispatch("decrement", { amount: this.changeRange.decrement });
+    }
   }
 }
 </script>
